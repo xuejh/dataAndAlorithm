8fee67ec8e0b18156d7b8bb0001d7cce5c40447b
xuejinhui <327646128@qq.com>
2023-07-25 17:17:57 +0800

    提交代码

--- eclipse-workspace/00-leetcode/src/二叉树/_94_二叉树的中序遍历.java
+++ eclipse-workspace/00-leetcode/src/二叉树/_94_二叉树的中序遍历.java
@@ -1,7 +1,42 @@
 package 二叉树;
 
-public class _94_二叉树的中序遍历 {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
 
+public class _94_二叉树的中序遍历 {
+	
+//	private void inorder(TreeNode root) {
+//		if(root == null) return;
+//		inorder(root.left);
+//		list.add(root.val);
+//		inorder(root.right);
+//	}
+//	private List<Integer> list = new LinkedList<>();
+//	
+//	public List<Integer> inorderTraversal(TreeNode root) {
+//		inorder(root);
+//		return list;
+//    }
+	
+	public List<Integer> inorderTraversal(TreeNode root) {
+		List<Integer> list = new ArrayList<>();
+		
+		Stack<TreeNode> stack = new Stack<>();
+		if(root != null) {
+			stack.push(root);
+		}
+		
+		while (stack.size() != 0) {
+			TreeNode node = stack.peek();
+			if(node.left != null) {
+				stack.push(node.left);
+			}
+			
+		}
+    }
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
