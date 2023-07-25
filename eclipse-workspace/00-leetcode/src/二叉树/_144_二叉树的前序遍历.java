5d948cf044dba30f5e234a2e4c9da9df8ae679fb
xuejinhui <327646128@qq.com>
2023-07-25 16:48:22 +0800

    代码提交

--- eclipse-workspace/00-leetcode/src/二叉树/_144_二叉树的前序遍历.java
+++ eclipse-workspace/00-leetcode/src/二叉树/_144_二叉树的前序遍历.java
@@ -1,21 +1,13 @@
 package 二叉树;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class _144_二叉树的前序遍历 {
-	 public class TreeNode {
-		  int val;
-		 TreeNode left;
-		 TreeNode right;
-		 TreeNode() {}
-		 TreeNode(int val) { this.val = val; }
-		 TreeNode(int val, TreeNode left, TreeNode right) {
-			 this.val = val;
-			 this.left = left;
-			 this.right = right;
-		 	}
-		 }
+
 //	 private List<Integer> list = new ArrayList<>();
 //	 
 //	 private void preorder(TreeNode root) {
@@ -30,18 +22,40 @@ public class _144_二叉树的前序遍历 {
 //		 	return list;
 //		 	
 //	    }
-	 public List<Integer> preorderTraversal(TreeNode root) {
+	 public static List<Integer> preorderTraversal(TreeNode root) {
 		 	List<Integer> list = new ArrayList<>();
-		 	TreeNode node = root;
-		 	while (node!=null) {
+		 	if(root == null) {
+		 		return list;
+		 	}
+		 	Queue<TreeNode> queue = new LinkedList<>();
+		 	
+		 	queue.offer(root);
+		 	while (queue.size()!=0) {
+				TreeNode node = queue.poll();
 				list.add(node.val);
-				if(node.left!=null) {
-					node = node.left;
+				if(node.left!= null) {
+					queue.offer(node.left);
 				}
-				
+				if(node.right!= null) {
+					queue.offer(node.right);
+				}
 			}
 		 	
 		 	return list;
 		 	
 	    }
+	 
+	 public static void main(String[] args) {
+		 
+		 TreeNode node0 = new TreeNode(1);
+		 TreeNode node1 = new TreeNode(4);
+		 TreeNode node2 = new TreeNode(3);
+		 TreeNode node3 = new TreeNode(2);
+		 node0.left = node1;
+		 node0.right = node2;
+		 node1.left = node3;
+		 
+		 
+		 System.out.println(preorderTraversal(node0));
+	}
 }
