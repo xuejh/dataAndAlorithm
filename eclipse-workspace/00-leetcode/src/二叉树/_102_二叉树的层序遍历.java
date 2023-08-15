16e45e5b7c276c4b15948cfc038969c2ca34028f
xuejinhui <327646128@qq.com>
2023-08-15 17:49:31 +0800

    层次遍历我还没搞懂

--- eclipse-workspace/00-leetcode/src/二叉树/_102_二叉树的层序遍历.java
+++ eclipse-workspace/00-leetcode/src/二叉树/_102_二叉树的层序遍历.java
@@ -8,19 +8,43 @@ import java.util.Stack;
 
 public class _102_二叉树的层序遍历 {
 	
-	private List<List<Integer>> lists = new ArrayList<>();
-	public List<List<Integer>> levelOrder(TreeNode root) {
-		if(root == null) return lists;
-		Queue<TreeNode> queue = new LinkedList<>();
-		queue.offer(root);
-		while (!queue.isEmpty()) {
-			TreeNode node = queue.poll();
-			if(node.left != null) {
-				queue.offer(node.left);
-			}
-			if(node.right != null) {
-				queue.offer(node.right);
-			}
+	public static void levelOrder(TreeNode node) {
+		if(node == null) {
+			return;
 		}
-    }
+		
+		Stack<TreeNode> stack = new Stack<>();
+		stack.push(node);
+		while (!stack.isEmpty()) {
+			TreeNode node1 = stack.pop();
+			System.out.println(node1.val + " ");
+			if(node1.right!=null) {
+				stack.push(node1.right);
+			}
+			if(node1.left !=null) {
+				stack.push(node1.left);
+			}
+			
+			
+		}
+	}
+	
+	public static void main(String[] args) {
+		TreeNode node0 = new TreeNode(3);
+		TreeNode node1 = new TreeNode(9);
+		TreeNode node2 = new TreeNode(20);
+		TreeNode node3 = new TreeNode(15);
+		TreeNode node4 = new TreeNode(7);
+		TreeNode node5 = new TreeNode(30);
+		TreeNode node6 = new TreeNode(40);
+		
+		node0.left = node1;
+		node0.right = node2;
+		node2.left = node3;
+		node2.right = node4;
+		node1.left = node5;
+		node1.right = node6;
+		
+		levelOrder(node0);
+	}
 }
