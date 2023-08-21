6c790497d2839ad245f66470877dce719da28cc1
xuejinhui <327646128@qq.com>
2023-08-21 10:34:06 +0800

    题目

---
+++ eclipse-workspace/00-leetcode/src/二叉树/PTreeNode.java
@@ -0,0 +1,10 @@
+package 二叉树;
+
+public class PTreeNode {
+	TreeNode parent;
+	TreeNode node;
+	public  PTreeNode(TreeNode parent,TreeNode node) {
+		this.parent = parent;
+		this.node = node;
+	}
+}
--- eclipse-workspace/00-leetcode/src/二叉树/_450_删除二叉搜索树中的节点.java
+++ eclipse-workspace/00-leetcode/src/二叉树/_450_删除二叉搜索树中的节点.java
@@ -5,68 +5,71 @@ import java.util.Stack;
 /*
  * https://leetcode.cn/problems/delete-node-in-a-bst/
  */
+
+
+
 public class _450_删除二叉搜索树中的节点 {
 	
 	public static TreeNode deleteNode(TreeNode root, int key) {
 		if(root == null) return null;
 		//查找节点
-		Stack<TreeNode> stack = new Stack<>();
-		stack.push(root);
-		TreeNode searchNode = null;
-		TreeNode parent = null;
+		Stack<PTreeNode> stack = new Stack<>();
+		
+		PTreeNode pNode = new PTreeNode(null, root);
+		stack.push(pNode);
+		PTreeNode searchNode = null;
 		while (!stack.isEmpty()) {
-			TreeNode node = stack.pop();
-			if(node.val == key) {
+			PTreeNode node = stack.pop();
+			if(node.node.val == key) {
 				searchNode = node;
-				parent = node;
 				break;
 			}
 			
-			if(node.right!= null) {
-				stack.push(node.right);
+			if(node.node.right!= null) {
+				stack.push(new PTreeNode(node.node, node.node.right));
 			}
-			if(node.left!= null) {
-				stack.push(node.left);
+			if(node.node.left!= null) {
+				stack.push(new PTreeNode(node.node, node.node.left));
 			}
 		}
 		
-		if(searchNode == null) return searchNode;
+		if(searchNode == null) return root;
 		//叶子节点直接删除
-		if(searchNode.left == null && searchNode.right == null) {
-			if(parent != null) {
-				if(parent.left == searchNode) {
-					parent.left = null;
+		if(searchNode.node.left == null && searchNode.node.right == null) {
+			if(searchNode.parent != null) {
+				if(searchNode.parent.left == searchNode.node) {
+					searchNode.parent.left = null;
 				}else {
-					parent.right = null;
+					searchNode.parent.right = null;
 				}
 			}else {
 				return null;
 			}
-		}else if (searchNode.left == null) {
-			TreeNode node = searchNode.right;
-			parent = searchNode;
+		}else if (searchNode.node.left == null) {
+			TreeNode node = searchNode.node.right;
+			TreeNode parent = searchNode.node;
 			while (node.left!= null) {
 				parent = node;
 				node = node.left;
 				
 			}
-			searchNode.val = node.val;
-			if(parent == searchNode) {
+			searchNode.node.val = node.val;
+			if(parent == searchNode.node) {
 				parent.right = null;
 			}else {
 				parent.left = null;
 			}
 			
 		}else {
-			TreeNode node = searchNode.left;
-			parent = searchNode;
+			TreeNode node = searchNode.node.left;
+			TreeNode parent = searchNode.node;
 			while (node.right!= null) {
 				parent = node;
 				node = node.right;
 				
 			}
-			searchNode.val = node.val;
-			if(parent == searchNode) {
+			searchNode.node.val = node.val;
+			if(parent == searchNode.node) {
 				parent.left = null;
 			}else {
 				parent.right = null;
@@ -76,20 +79,20 @@ public class _450_删除二叉搜索树中的节点 {
     }
 	
 	public static void main(String[] args) {
-		TreeNode node0 = new TreeNode(5);
-		TreeNode node1 = new TreeNode(3);
-		TreeNode node2 = new TreeNode(6);
-		TreeNode node3 = new TreeNode(2);
-		TreeNode node4 = new TreeNode(4);
-		TreeNode node5 = new TreeNode(7);
-		
-		node0.left = node1;
-		node0.right = node2;
-		node1.left = node3;
-		node1.right = node4;
-		node2.right = node5;
-		
-		deleteNode(node0,3);
+//		TreeNode node0 = new TreeNode(5);
+//		TreeNode node1 = new TreeNode(3);
+//		TreeNode node2 = new TreeNode(6);
+//		TreeNode node3 = new TreeNode(2);
+//		TreeNode node4 = new TreeNode(4);
+//		TreeNode node5 = new TreeNode(7);
+//		
+//		node0.left = node1;
+//		node0.right = node2;
+//		node1.left = node3;
+//		node1.right = node4;
+//		node2.right = node5;
+		TreeNode node0 = new TreeNode(0);
+		deleteNode(node0,0);
 	}
 	
 }
